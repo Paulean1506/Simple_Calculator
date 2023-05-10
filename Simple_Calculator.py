53a8f40fa79acb312d60fc6d01bf4ed0f4081c84
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-05-10 16:00:30 +0800

    import figlet, termcolor, and table

---
+++ Simple_Calculator.py
@@ -0,0 +1,8 @@
+# Paulean Marguerette F. Parrish
+# Bscpe 1-4
+# Simple Calculator activity
+
+# Import everything needed for design
+from termcolor import colored
+from pyfiglet import Figlet
+from prettytable import PrettyTable
